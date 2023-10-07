6377910bf2ed990c4f5b8545b67f992c158b7522
aleflammia <aflammia@correo.unimet.edu.ve>
2023-10-07 14:27:47 -0400

    creacion clase dev

---
+++ src/flammiaramos/Dev.java
@@ -0,0 +1,49 @@
+
+package flammiaramos;
+
+import static java.lang.Thread.sleep;
+import java.util.concurrent.Semaphore;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Dev extends Thread {
+    private String text;
+    private String type;
+    private float prodByDay;
+    private float counter;
+    private int wage;
+    private long savings;
+    private Semaphore mutex;
+
+    public Dev(String text, Semaphore mutex, String type, float production, int wage){
+        this.text = text;
+        this.mutex = mutex;
+        this.type = type;
+        this.counter = 0;
+        this.prodByDay = production;
+        this.savings = 0;
+        this.wage = wage;
+    }
+
+    @Override
+    public void run(){
+        while(true){
+            try{
+
+                mutex.acquire();
+                savings+=wage;
+                counter += this.prodByDay;
+
+                System.out.println(text + counter + " con:  " + savings + "$");
+                
+                sleep(500);         
+                mutex.release();
+                sleep(1000);
+            }
+            catch(InterruptedException ex){
+                Logger.getLogger(Dev.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+
+}
--- src/flammiaramos/Main.java
+++ src/flammiaramos/Main.java
@@ -12,23 +12,23 @@ public class Main {
     public static void main(String[] args) {
         // TODO code application logic here
         Semaphore mainMutex = new Semaphore(1);
-        Hilo narrativa = new Hilo("Narrativa: ", mainMutex);
-        Hilo niveles = new Hilo("Niveles: ", mainMutex);
-        Hilo sprites = new Hilo("Sprites: ", mainMutex);
-        Hilo logica = new Hilo("Logica: ", mainMutex);
-        Hilo DLC = new Hilo("DLC: ", mainMutex);
-        Hilo integrador = new Hilo("Integrador: ", mainMutex);
-        Hilo PM = new Hilo("PM: ", mainMutex);
-        Hilo director = new Hilo("Director: ", mainMutex);
+        Dev narrativa = new Dev("Guiones producidos: ", mainMutex, "narrativa", 0.5f, 240);
+        Dev niveles = new Dev("Niveles producidos: ", mainMutex, "niveles", 0.5f, 312);
+        Dev sprites = new Dev("Sprites sets producidos: ", mainMutex, "sprites", 1f, 480);
+        Dev logica = new Dev("Sistemas de juego producidos: ", mainMutex, "logica", 5f, 192);
+        Dev DLC = new Dev("DLC producidos: ", mainMutex, "DLC", 0.5f, 408);
+//        Dev integrador = new Dev("Integrador: ", mainMutex);
+//        Dev PM = new Dev("PM: ", mainMutex);
+//        Dev director = new Dev("Director: ", mainMutex);
 
         narrativa.start();
         niveles.start();
         sprites.start();
         logica.start();
         DLC.start();
-        integrador.start();
-        PM.start();
-        director.start();
+//        integrador.start();
+//        PM.start();
+//        director.start();
     
     }
     
