f4c08c7b4d95cca5d6eb3d01cd40a538c60de4bd
aleflammia <aflammia@correo.unimet.edu.ve>
2023-10-09 15:11:32 -0400

    creacion calse Empresa

---
+++ src/flammiaramos/Empresa.java
@@ -0,0 +1,10 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package flammiaramos;
+
+public class Empresa {
+    public String name;
+    public int diasEntrega, diasRestantes;
+}
